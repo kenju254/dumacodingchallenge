8a7b134b80f590399b0b00457d473f2b9d351766
kenju254 <kenmbugua@gmail.com>
2014-01-08 15:19:07 +0300

    Added Script to insert data into the db and implemented it using django-admin tools

--- places/management/commands/load_data.py
+++ places/management/commands/load_data.py
@@ -10,53 +10,68 @@ import csv
 #setup_environ(settings)
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
+from django.core.management.base import BaseCommand
 from places.models import County, Ward, Location
 
 county_data = open(os.path.join(os.path.dirname(__file__),'csvdata','userdata_county.csv' ))
 ward_data = open(os.path.join(os.path.dirname(__file__), 'csvdata', 'userdata_ward.csv'))
 location_data = open(os.path.join(os.path.dirname(__file__), 'csvdata', 'userdata_location.csv'))
 
-'''Loading Counties into the Database'''
-def insert_counties():
-    with county_data:
-        reader = csv.DictReader(county_data, fieldnames=['name','num'])
-        for row in reader:
-            county = County(name=row['name'],
-                            num=row['num'])
-            county.save()
+class Command(BaseCommand):
+
+    def insert_counties(self):
+        ''' Loading Counties into the Database'''
+        with county_data:
+            self.reader = csv.DictReader(county_data, fieldnames=['name','num'])
+            for row in self.reader:
+                self.county = County(name=row['name'],
+                    num=row['num'])
+                self.county.save()
 
 
-'''Loading Wards into the Database'''
-def insert_wards():
-    with ward_data:
-        reader = csv.DictReader(ward_data, fieldnames=['num','name', 'countynum'])
-        for row in reader:
-            ward = Ward(name=row['name'],
-                        num=row['num'])
 
-        #Try catch block connects the countynum to the ward table
-            try:
-                ward.county = County.objects.get(num=row['countynum'])
-            except:
-                pass
-            else:
-                ward.save
+    def insert_wards(self):
+        '''Loading Wards into the Database'''
+        with ward_data:
+            self.reader = csv.DictReader(ward_data, fieldnames=['num','name', 'countynum'])
+            for row in self.reader:
+                self.ward = Ward(name=row['name'],
+                    num=row['num'])
+
+                #Try catch block connects the countynum to the ward table
+                try:
+                    self.ward.county = County.objects.get(num=row['countynum'])
+                except:
+                    pass
+                else:
+                    self.ward.save()
 
 
-'''Loading Locations into the Database'''
-def insert_locations():
-    with location_data:
-        reader = csv.DictReader(location_data, fieldnames=['num','name','wardnum', 'countynum','latitude','longitude'])
-        for row in reader:
-            location = Location(name=row['name'],
-                                num=row['num'],
-                                latitude=row['latitude'],
-                                longitude=row['longitude'])
-            try:
-                location.ward = Ward.objects.get(num=row['wardnum'])
-            except:
-                pass
-            else:
-                location.save()
+
+    def insert_locations(self):
+        '''Loading Locations into the Database'''
+        with location_data:
+            self.reader = csv.DictReader(location_data, fieldnames=['num','name','wardnum', 'countynum','latitude','longitude'])
+            for row in self.reader:
+                self.location = Location(name=row['name'],
+                                        num=row['num'],
+                                        latitude=row['latitude'],
+                                        longitude=row['longitude'])
+                try:
+                    self.location.ward = Ward.objects.get(num=row['wardnum'])
+                except:
+                    pass
+                else:
+                    self.location.save()
+
+
+
+
+
+    def handle(self, *args, **options):
+        self.insert_counties()
+        self.insert_wards()
+        self.insert_locations()
+
 
 
